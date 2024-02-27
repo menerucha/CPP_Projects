208e54ac57c63f8f95ae72c47c0ec3a11d63ebd7
menerucha <menerucha2004@gmail.com>
2024-02-27 18:41:15 +0530

    Project_02

---
+++ Library_Management_System.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    cout << "--------------------------";
+    cout << "1 - Show All Books";
+    cout << "2-Extract Books";
+    cout << "3- Add books(ADMIN)";
+    cout << "4- Exit";
+    cout << "--------------------------";
+    cout << "Enter Your Choice ::";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case '1':
+        break;
+    case '2':
+        break;
+    case '3':
+        break;
+    case '4':
+        break;
+    default:
+        cout << "Invalid Choice..!";
+    }
+}
